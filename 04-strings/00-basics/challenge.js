f8b7722e36889c66440a2f522e83d1e6b4f095de
Thomas Hils <thomas@hils.us>
2014-06-24 12:45:46 +0000

    solved strings challenge.js

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,25 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+  return "Hello " + firstName + "," + "\n\n" + message + "\n\nSincerely," + "\n" + senderName;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
+  var a = bigString.substring(startA, endA);
+  var b = bigString.substring(startB, endB);
+  return a+b;
 
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+  return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+  return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+  var softStart = text.indexOf(searchString);
+  var hardStart = softStart + searchString.length;
+  var softStop = text.lastIndexOf(searchString);
+  return text.substring(hardStart, softStop);
 };
