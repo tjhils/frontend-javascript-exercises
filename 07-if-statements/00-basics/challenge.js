01b666013e74684d657e1191274ba3cc5f2de6b0
Thomas Hils <thomas@hils.us>
2014-06-26 13:43:36 +0000

    if else challenge.js complete

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,46 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(fav, guess){
+  if (guess > fav) {
+      return "Too high";
+  } else if (guess < fav) {
+    return "Too low";
+  } else {
+    return "You got it!";
+  }
+};
+  
 
-module.exports.checkLock = undefined;
+module.exports.checkLock = function(first, second, third, fourth) {
+  if (first == 3 || first == 5 || first == 7) {
+    if (second == 2) {
+      if (third >= 5 && 100 >= third) {
+        if (fourth < 9 || 20 < fourth) {
+          return "correct";
+        } else {
+          return "incorrect";
+        }
 
-module.exports.canIGet = undefined;
+      } else {
+          return "incorrect";
+      }
+
+    } else {
+      return "incorrect";
+    }
+  } else {
+    return "incorrect";
+  }
+};
+
+module.exports.canIGet = function(item, price){
+  if (item == "MacBook Air" && price >= 999) {
+    return true;
+  } else if (item == "MacBook Pro" && price >= 1299) {
+    return true;
+  } else if (item == "Mac Pro" && price >= 2499) {
+    return true;
+  } else if (item == "Apple Sticker" && price >= 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
