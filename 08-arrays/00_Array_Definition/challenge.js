3cbbc89b4f3f8a21c53dc15515c23bde540f9612
Thomas Hils <thomas@hils.us>
2014-06-28 14:12:18 +0000

    solved array definition challenge.js

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,13 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a, b, c, d){
+    var theArray = [a, b, c, d]
+    return theArray;
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(inputArray){
+    var lastElementIndex = inputArray.length - 1;
+    var answerEnd = (inputArray[lastElementIndex]);
+    var answerStart = (inputArray[0]);
+    var finalArray = [answerStart, answerEnd];
+    return finalArray;
+    
+};
